6497317fe6a6b92f3efba8c27465393f67a2d696
KidusTadesseCode <Kidustadessearega@gmail.com>
2022-05-31 19:37:34 -0400

    commit

--- dist/index.js
+++ dist/index.js
@@ -1,100 +1,60 @@
 'use strict';
 
-var styled = require('styled-components');
+var PropTypes = require('prop-types');
 
 function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-var styled__default = /*#__PURE__*/_interopDefaultLegacy(styled);
+var PropTypes__default = /*#__PURE__*/_interopDefaultLegacy(PropTypes);
 
-const Toggle = styled__default["default"].input.attrs({
-  type: 'checkbox'
-})`
-    position: relative;
-    width: ${({
-  width
-}) => width + "px"};
-    height: ${({
-  width
-}) => width / 3 + "px"};
-    margin: 10px;
-    outline: none;
-    background: ${({
-  bgOff
-}) => bgOff};
-    -webkit-appearance: none;
-    cursor: pointer;
-    border-radius: 20px;
-    box-shadow: -5px -5px 20px rgba(255, 255, 255, 0.1),
-                5px 5px 10px rgba(0, 0, 0, 1),
-                inset -2px -2px 5px rgba(255, 255, 255, 0.1),
-                inset 2px 2px 5px rgba(255, 255, 255, 0.5),
-                0 0 0 2px #1f1f1f;
-    transition: 0.5s;
-    &:checked {
-        //background: #20b7ff;
-        background: ${({
-  bgOn
-}) => bgOn};
-    }
+function styleInject(css, ref) {
+  if ( ref === void 0 ) ref = {};
+  var insertAt = ref.insertAt;
 
-    &::before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: ${({
-  width
-}) => width / 3 + "px"};
-        width:${({
-  width
-}) => width / 1.5 + "px"};
-        background: ${({
-  switchbg
-}) => switchbg};
-        border-radius: 20px;
-        box-shadow: 0 0 0 1px #232323;
-        transform: scale(0.98, 0.96);
-        transition: 0.5s;
-    }
-    &:checked::before {
-        left: ${({
-  width
-}) => width / 3 + "px"};
-    }
-    &:after {
-        content: '';
-        position: absolute;
-        left: ${({
-  width
-}) => width / 1.8 + "px"};
-        top: calc(50% - 2px);       
-        height: ${({
-  width
-}) => width / 30 + "px"};
-        width: ${({
-  width
-}) => width / 30 + "px"};
-        border-radius: 50%;
-        background: ${({
-  bgOff
-}) => bgOff};
-        transition: 0.5s;
-    }
-    &:checked::after {
-        left: ${({
-  width
-}) => width / 1.8 + width / 3 + "px"};
-        background: ${({
-  bgOn
-}) => bgOn};
-        box-shadow: 0 0 5px ${({
-  bgOn
-}) => bgOn}, 0 0 15px ${({
-  bgOn
-}) => bgOn}, 0 0 30px ${({
-  bgOn
-}) => bgOn};
-    }
-`;
+  if (!css || typeof document === 'undefined') { return; }
 
-module.exports = Toggle;
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+var css_248z = "";
+styleInject(css_248z);
+
+function AccordionTitle({
+  left,
+  center,
+  right
+}) {
+  return /*#__PURE__*/React.createElement("div", {
+    class: "container"
+  }, /*#__PURE__*/React.createElement("div", {
+    class: "left"
+  }, left), /*#__PURE__*/React.createElement("div", {
+    class: "center"
+  }, center), /*#__PURE__*/React.createElement("div", {
+    class: "right"
+  }, right));
+}
+AccordionTitle.PropTypes = {
+  left: PropTypes__default["default"].oneOfType([PropTypes__default["default"].string, PropTypes__default["default"].element]),
+  center: PropTypes__default["default"].oneOfType([PropTypes__default["default"].string, PropTypes__default["default"].element]),
+  right: PropTypes__default["default"].oneOfType([PropTypes__default["default"].string, PropTypes__default["default"].element])
+};
+
+module.exports = AccordionTitle;
